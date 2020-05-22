4cc609215ad7859d9a6781491deb17f37fbe7bfe
Tenaysia Fox <tenaysiafox@gmail.com>
2020-05-22 22:26:04 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,7 +1,7 @@
 def find_max_value(array)
  i = 0
  while i < array.length do
-   if array[i] > array[i-1]
+   if array[i] > array[i-1] && array[i] <= array[i+1]
      return array[i]
    end
    i += 1
